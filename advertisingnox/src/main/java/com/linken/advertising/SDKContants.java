4156173e1f0450a433e981c5c3f8dd50b0e3913c
liuxihui <liuxihui168168@163.com>
2019-07-31 16:07:37 +0800

    混淆 andorid x

--- advertisingnox/src/main/java/com/linken/advertising/SDKContants.java
+++ advertisingnox/src/main/java/com/linken/advertising/SDKContants.java
@@ -6,7 +6,7 @@ public class SDKContants {
 
     public static final String BASE_URL = BuildConfig.BASE_URL;
     public static final String URL_DISTRIBUTEAD = BASE_URL + "/ad/c/distributeAd";
-    public static final String URL_COMPLETE_AD = "ad/c/complete";
+    public static final String URL_COMPLETE_AD = "/ad/c/complete";
 //    public static final String URL_DISTRIBUTEAD = BASE_URL + "c/istributeAd";
 
 
